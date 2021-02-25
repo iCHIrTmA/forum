49ec10485bc9f65a5133b98cabf67938aeb2a4e1
Janroe <janroe@nabepero.co.jp>
2021-02-25 08:56:33 +0800

    14:10

--- resources/views/profiles/activities/created_reply.blade.php
+++ resources/views/profiles/activities/created_reply.blade.php
@@ -1,16 +1,8 @@
-<div class="card">
-    <div class="card-header">
-    	<div class="level">
-    		<span class="flex">
-    			{{ $profileUser->name }} replied to a 
-    			<a href="{{ url($activity->subject->thread->path())}}"> "{{ $activity->subject->thread->title }}" </a>
-	        </span>
-
-            <span> {{-- {{ $thread->created_at->diffForHumans() }}  --}}</span>
-        </div>
-    </div>
-
-    <div class="card-body">
-        {{ $activity->subject->body }}
-    </div>
-</div>
+@component('profiles.activities.activity')
+	@slot('heading')
+		{{ $profileUser->name }} replied to a <a href="{{ url($activity->subject->thread->path())}}"> "{{ $activity->subject->thread->title }}" </a>
+	@endslot
+	@slot('body')
+	    {{ $activity->subject->body }}
+	@endslot
+@endcomponent
--- resources/views/profiles/activities/created_thread.blade.php
+++ resources/views/profiles/activities/created_thread.blade.php
@@ -1,15 +1,8 @@
-<div class="card">
-    <div class="card-header">
-    	<div class="level">
-    		<span class="flex">
-    			{{ $profileUser->name }} published <a href="{{ url($activity->subject->path()) }}">{{ $activity->subject->title }}</a>
-	        </span>
-
-            <span> {{-- {{ $thread->created_at->diffForHumans() }}  --}}</span>
-        </div>
-    </div>
-
-    <div class="card-body">
+@component('profiles.activities.activity')
+    @slot('heading')
+        {{ $profileUser->name }} published <a href="{{ url($activity->subject->path()) }}">{{ $activity->subject->title }}</a>
+    @endslot
+    @slot('body')
         {{ $activity->subject->body }}
-    </div>
-</div>
+    @endslot
+@endcomponent
